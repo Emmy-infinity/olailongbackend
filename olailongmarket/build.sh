90456c822b4a5c59d5ee39b1ed85f58c390be0bf
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-09-01 19:53:26 +0300

    Add build script for project setup

---
+++ olailongmarket/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
